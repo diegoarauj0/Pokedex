6c2fa4f7f05f7e6f6dd4e5020593418da56742e5
Diego Araujo <97152724+diegoarauj0@users.noreply.github.com>
2024-02-11 19:26:01 -0300

    Update usePokemons.ts

--- src/hook/usePokemons.ts
+++ src/hook/usePokemons.ts
@@ -4,6 +4,7 @@ import client from "../pokenode"
 
 function isValidJson(jsonString:string): boolean {
     try {
+        JSON.parse(jsonString)
         return true
     } catch { return false }
 }
